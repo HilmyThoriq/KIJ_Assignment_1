3f1572b7d4095ceb5ae94aae9b0d11c0e954abc3
Hilmy Thoriq <hilmythoriq09@gmail.com>
2022-10-04 12:52:03 +0900

    Remake Encryption with AES

--- rc4/decrypt_rc4.py
+++ rc4/decrypt_rc4.py
@@ -3,7 +3,7 @@ from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 
 key = b"sapodkfpasokdfpakwp0ro2-o4-19-053c.=waxCAEr//.;\DQW/E]WQ["
-msg = b'\xfeO\xd9\xb2'
+msg = b'\x0e)\x92 '
 
-decrypt = ARC4.new(key).decrypt(msg)
-print(decrypt.decode("utf-8"))
+decrypt = ARC4.new(key).decrypt(msg.decode('utf-8'))
+print(decrypt)
